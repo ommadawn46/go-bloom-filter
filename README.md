5bfde79652ba036cc4faf3e87aa5b8e611f26eba
ommadawn46 <ommadawn46@gmail.com>
2019-03-08 15:27:45 +0900

    add test

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ FN: 0.00000000
 FP Predict: 0.00001002
 FP Actual: 0.00001333
 ```
+
+```
+❯ go test -bench . -benchmem
+goos: darwin
+goarch: amd64
+pkg: github.com/ommadawn46/go-bloom-filter/bloomfilter
+BenchmarkAdd-4                 2         892253683 ns/op        13848856 B/op    1700009 allocs/op
+BenchmarkContains-4           10         223782231 ns/op         3481412 B/op     435089 allocs/op
+PASS
+ok      github.com/ommadawn46/go-bloom-filter/bloomfilter       6.053s
+```
---
+++ bloomfilter/bloomfilter_test.go
@@ -0,0 +1,39 @@
+package bloomfilter
+
+import (
+	"fmt"
+	"testing"
+)
+
+var p float64
+var m, k, n uint
+var bf *BloomFilter
+
+func init() {
+	p, n = 1e-4, uint(100000)
+	m = OptimizeM(p, n)
+	k = OptimizeK(m, n)
+	bf = NewBloomFilter(m, k)
+	for i := uint(0); i < n; i++ {
+		bf.Add([]byte(fmt.Sprint(i)))
+	}
+}
+
+func BenchmarkAdd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		tmpBf := NewBloomFilter(m, k)
+		baseStr := fmt.Sprint(i)
+		for j := uint(0); j < n; j++ {
+			tmpBf.Add([]byte(baseStr + fmt.Sprint(j)))
+		}
+	}
+}
+
+func BenchmarkContains(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		baseStr := fmt.Sprint(i)
+		for j := uint(0); j < n; j++ {
+			bf.Contains([]byte(baseStr + fmt.Sprint(j)))
+		}
+	}
+}
